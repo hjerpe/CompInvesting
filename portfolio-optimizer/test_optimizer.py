c9b4cd167c293196f2c7224516e7ad871f1b62c6
hjerpe <hjerpe.adam@gmail.com>
2016-02-14 17:08:38 +0100

    refactored test function

--- portfolio-optimizer/test_optimizer.py
+++ portfolio-optimizer/test_optimizer.py
@@ -2,50 +2,78 @@ from optimizer import optimizer, print_optimization, plot_performance
 import datetime as dt
 
 
-def print_and_test_optimization():
+def print_and_test_optimization(dt_start, dt_end, arr_stock_symbols,
+        true_sharpe, true_vol, true_average_ret, true_cum_ret, true_allocation):
 
     def check_metrics(
             sharpe, vol, average_ret, cum_ret, allocation,
-            cor_sharpe, cor_vol, cor_average_ret, cor_cum_ret, cor_allocation):
+            true_sharpe, true_vol, true_average_ret, true_cum_ret, 
+            true_allocation):
         print("deviance from truth, sharpe ratio: {b}".format(
-            b=sharpe - cor_sharpe))
+            b=sharpe - true_sharpe))
         print("deviance from truth, vol: {b}".format(
-            b=vol - cor_vol))
+            b=vol - true_vol))
         print("deviance from truth, average daily ret: {b}".format(
-            b=average_ret - cor_average_ret))
+            b=average_ret - true_average_ret))
         print("deviance from truth, cum ret: {b}".format(
-            b=cum_ret - cor_cum_ret))
+            b=cum_ret - true_cum_ret))
         print("deviance from truth, allocation {b}".format(
-            b=allocation - cor_allocation))
-            
-    print("\nFirst test")
-    arr_symbols = ["AAPL", "GLD", "GOOG", "XOM"]
-    dt_start = dt.datetime(2011, 1, 1)
-    dt_end = dt.datetime(2011, 12, 31)
-    print_optimization(dt_start, dt_end, arr_symbols)
-    allocations, arr_met = optimizer(dt_start, dt_end, arr_symbols)
-    v1 = 1.02828403099
-    v2 = 0.0101467067654
-    v3 = 0.000657261102001
-    v4 = 1.16487261965
-    v5 = [.4, .4, 0, .2]
-    print("\nDeviations from truth:")
+            b=allocation - true_allocation))
+
+        
+    str_start_dt = "date_start: %s" % dt_start
+    str_end_dt = "date_end: %s" % dt_end
+    str_stock_symbols = "stock symbols: %s" % arr_stock_symbols
+    print("\nTest\n{dt_start}\n{dt_end}\n{symbols}".format(
+        dt_start=str_start_dt,
+        dt_end=str_end_dt,
+        symbols=str_stock_symbols))
+    allocations, arr_met = optimizer(dt_start, dt_end, arr_stock_symbols)
     check_metrics(arr_met[3], arr_met[2], arr_met[1], arr_met[0], allocations,
-            v1, v2, v3, v4, v5)
-    plot_performance(dt_start, dt_end, arr_symbols, allocations)
+            true_sharpe, true_vol, true_average_ret, true_cum_ret, 
+            true_allocation)
+    plot_performance(dt_start, dt_end, arr_stock_symbols, allocations)
     
-    print("\nSecond test")
-    arr_symbols = ['AXP', 'HPQ', 'IBM', 'HNZ']
-    dt_start = dt.datetime(2010, 1, 1)
-    dt_end = dt.datetime(2010, 12, 31)
-    print_optimization(dt_start, dt_end, arr_symbols)
-    allocations, arr_met = optimizer(dt_start, dt_end, arr_symbols)
-    v1 = 1.29889334008
-    v2 = 0.00924299255937
-    v3 = 0.000756285585593
-    v4 = 1.1960583568
-    v5 = [0, 0, 0, 1]
-    print("\nDeviations from truth:")
-    check_metrics(arr_met[3], arr_met[2], arr_met[1], arr_met[0], allocations,
-            v1, v2, v3, v4, v5)
-    plot_performance(dt_start, dt_end, arr_symbols, allocations)
+
+
+def main(dic_input, dic_true_metrics):
+
+    print_and_test_optimization(dic_input["dt_start"], dic_input["dt_end"], 
+            dic_input["arr_stock_symbols"], 
+            dic_true_metrics["sharpe"], dic_true_metrics["vol"],
+            dic_true_metrics["average_ret"], dic_true_metrics["cum_ret"],
+            dic_true_metrics["allocation"])
+
+
+if __name__ == "__main__":
+    arr_dic_inputs = []
+    arr_dic_true_metrics = []
+    # Add test case
+    arr_dic_inputs.append(
+            {"dt_start": dt.datetime(2011, 1, 1),
+            "dt_end": dt.datetime(2011, 12, 31),
+            "arr_stock_symbols": ["AAPL", "GLD", "GOOG", "XOM"]}
+            )
+    arr_dic_true_metrics.append(
+            {"sharpe": 1.02828403099,
+            "vol": 0.0101467067654,
+            "average_ret": 0.000657261102001,
+            "cum_ret": 1.16487261965,
+            "allocation": [.4, .4, 0, .2]}
+            )
+    # Add test case
+    arr_dic_inputs.append(
+            {"dt_start": dt.datetime(2010, 1, 1),
+            "dt_end": dt.datetime(2010, 12, 31),
+            "arr_stock_symbols": ['AXP', 'HPQ', 'IBM', 'HNZ']}
+            )
+    arr_dic_true_metrics.append(
+            {"sharpe": 1.29889334008,
+            "vol": 0.00924299255937,
+            "average_ret": 0.000756285585593,
+            "cum_ret": 1.1960583568,
+            "allocation": [0, 0, 0, 1]}
+            )
+    for dic_input, dic_true_metrics in zip(arr_dic_inputs,
+            arr_dic_true_metrics):
+        main(dic_input, dic_true_metrics)
