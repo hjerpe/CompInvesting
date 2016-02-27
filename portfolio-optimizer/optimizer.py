26496577d594ad0e7d537d19ef15ed88f8fea2ff
hjerpe <hjerpe.adam@gmail.com>
2016-02-27 12:29:25 +0100

    removed function dic_df_data from optimizer

--- portfolio-optimizer/optimizer.py
+++ portfolio-optimizer/optimizer.py
@@ -18,21 +18,6 @@ sys.path.append("./../get-market-data")
 from get_market_data import dic_df_data
 
 
-#def dic_df_data(date_start, date_end, arr_stock_symbols):
-#    '''Returns a dictionary with keys 'open', 'high, 'low', 'close',
-#    'volume', 'actual_close' and where each value is of data frame for
-#    every equity in arr_stock_symbols.'''
-#
-#
-#    date_time_of_day = dt.timedelta(hours=16)
-#    arr_date_time_stamps = du.getNYSEdays(date_start, date_end, date_time_of_day)
-#    conn_dataobj = da.DataAccess("Yahoo")
-#    arr_keys = ["open", "high", "low", "close", "volume", "actual_close"]
-#    arr_dfs_data = conn_dataobj.get_data(arr_date_time_stamps, arr_stock_symbols,
-#            arr_keys)
-#    return dict(zip(arr_keys, arr_dfs_data))
-
-
 def optimizer(date_start, date_end, arr_stock_symbols, weight_increment):
     '''Returns an optimal equity allocation over the equities in
     arr_stock_symbols w.r.t. the sharpe ratio.'''
